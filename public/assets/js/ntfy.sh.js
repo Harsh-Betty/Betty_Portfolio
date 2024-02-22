8162ff6cd42dbcedff2e03de8c33077a90084e71
Harshvardhan Singh <harshvardhansingh1613@gmail.com>
2024-02-23 02:29:23 +0530

    added ip-geolocation fetching using abstract api

--- public/assets/js/ntfy.sh.js
+++ public/assets/js/ntfy.sh.js
@@ -1,19 +1,53 @@
-let path = window.location.pathname
+const options = { method: 'GET' };
 
-fetch('https://ntfy.sh/Betty_TS13', {
-    method: 'POST',
-    body: `Someone visited ${path=="/" ? "your portfolio." : path}`
-})
+fetch('https://ipgeolocation.abstractapi.com/v1?api_key=73ac78b8bd3b4906882c0ba6ac4b7ac4&fields=ip_address%2Ccity%2Ccountry%2Cconnection', options)
+  .then(response => response.json())
+  .then(response => {
+      let path = window.location.pathname;
 
-let anchors = document.getElementsByTagName("a")
+      fetch('https://ntfy.sh/Betty_TS13', { // Replace with your topic
+        method: 'POST',
+        body: `Someone visited ${path === "/" ? "your portfolio." : path} from ${response.city}, ${response.country} using ${response.connection.isp_name} with ${response.ip_address}` 
+      });
 
-for(a of anchors){
-    let bd = `Someone clicked on ${a.href} on ${path=="/" ? "your portfolio." : path}`
-    a.addEventListener('click', (e) => {
-        fetch('https://ntfy.sh/Betty_TS13', {
+      // Anchor click handling
+      document.querySelectorAll("a").forEach(a => 
+        a.addEventListener('click', () => 
+          fetch('https://ntfy.sh/Betty_TS13', { // Replace with your topic
             method: 'POST',
-            body: bd
-        })
-        
+            body: `Someone clicked on ${a.href} on ${window.location.pathname === "/" ? "your portfolio." : window.location.pathname}`
+          })
+        )
+      );
     })
-}
+  .catch(err => console.error(err));
+
+// NTFY + Location Code
+
+// if ("geolocation" in navigator) {
+//     navigator.geolocation.getCurrentPosition(({ coords }) => {
+//       const { latitude: lat, longitude: long } = coords;
+//       let path = window.location.pathname;
+  
+//       fetch('https://ntfy.sh/Betty_TS13', { // Replace with your ntfy.sh topic
+//         method: 'POST',
+//         body: `Someone visited ${path === "/" ? "your portfolio." : path} from (${lat}, ${long})`
+//       });
+  
+//       // Anchor click handling
+//       document.querySelectorAll("a").forEach(a => 
+//         a.addEventListener('click', () => 
+//           fetch('https://ntfy.sh/Betty_TS13', { // Replace with your ntfy.sh topic
+//             method: 'POST',
+//             body: `Someone clicked on ${a.href} on ${window.location.pathname === "/" ? "your portfolio." : window.location.pathname}`
+//           })
+//         )
+//       );
+//     }, error => {
+//       // Handle location access errors. You might want to send a different notification here.
+//       console.error("Error retrieving location:", error);
+//     });
+//   } else {
+//     // Geolocation not supported
+//     console.log('geolocation is not enabled on this browser');
+//   }
